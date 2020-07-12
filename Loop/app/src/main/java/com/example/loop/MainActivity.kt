cd881281a80d60a5404c002458c6c98a9e57e473
mysterico <ian@mystericoui-iMac.local>
2020-07-12 15:12:32 +0900

    .

--- Loop/app/src/main/java/com/example/loop/MainActivity.kt
+++ Loop/app/src/main/java/com/example/loop/MainActivity.kt
@@ -11,11 +11,15 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         /**
-         * 선행학습 요소
+         * 1. 텍스트 입력
+         * 2. 버튼을 누른다
+         * 3. 버튼을 누르면 editText의 입력값과 반복문을 이용하여 구구단을 생성한다
+         * 4. 반복문으로 생성한 문자열을 textView에 입력한다
          *
-         * 텍스트뷰 이해하기
-         * 텍스트뷰의 텍스트 가져오기
-         * 형변환 하기
+         * String 을 Int로 변경하는 법
+         *
+         * val text = editText.text.toString()
+         * val convertedNumber = Integer.parseInt(text)
          */
 
         startBtn.setOnClickListener {
