2f826303d1d5bf47648d6f0abe7d0bbda689f75d
mysterico <ian@mystericoui-iMac.local>
2020-07-25 03:04:35 +0900

    http

---
+++ Http/app/src/main/java/com/example/http/JokeService.kt
@@ -0,0 +1,32 @@
+package com.example.http
+
+import com.google.gson.annotations.SerializedName
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+/**
+ * 인터페이스를 이용하여 호출방식, 주소 그리고 데이터 등을 정의한다.
+ * 레트로핏은 인터페이스 해석하여 통신을 처리한다.
+ * 반환타입은 retrofit의 인터페이스 Call을 선택해야한다.
+ * 다른 클래스의 Call을 선택하지 않도록 주의해야한다.
+ */
+interface JokeService {
+    @GET("jokes/{id}")
+    fun getData(
+        @Path("id") jokeId: String
+    ): Call<JokeResponse>
+}
+
+/**
+ * 응답 받을 데이터 구조와 같은 구조이어야 하며 변수 명도 같아야한다.
+ */
+data class JokeResponse(
+    @SerializedName("value") val data: JokeValue
+)
+
+data class JokeValue(
+    @SerializedName("id") val id: String,
+    @SerializedName("joke") val joke: String,
+    @SerializedName("categories") val categories: Array<Any>
+)
--- Http/app/src/main/java/com/example/http/MainActivity.kt
+++ Http/app/src/main/java/com/example/http/MainActivity.kt
@@ -2,10 +2,123 @@ package com.example.http
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.io.BufferedInputStream
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.lang.Exception
+import java.net.URL
+import javax.net.ssl.HttpsURLConnection
+import kotlin.concurrent.thread
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        /**
+         * 1. manifiests 파일에 권한 추가 (추가후에도 안된다면 에뮬레이터에서 앱을 삭제 후 다시 실행)
+         * 2. 메인 스레드에서 통신 및 UI 작업을 하면 안되므로 새로운 스레드로 작업
+        * 3. retrofit을 사용하기 위해서 자바 버전 변석
+         * 4. gradle에 retrofit, gson 추가
+         */
+
+        editText.setText("https://api.icndb.com/jokes/random")
+        
+        libFlag.setOnCheckedChangeListener { compoundButton, b ->  libFlag.isChecked = b}
+
+        button.setOnClickListener {
+            try {
+                var textUrl = editText.text.toString()
+//            URL객체로 변환
+                var url = URL(textUrl)
+
+                if (libFlag.isChecked) {
+                    getDataByRetrofit()
+                } else {
+                    getDataByURLConnection(url)
+                }
+
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+
+        }
+    }
+
+    private fun getDataByRetrofit() {
+        try {
+            /**
+             * retrofit 객체생성
+             * 서버에서 데이터를 JSON으로 돌려주기 때문에 GsonConverterFactory.create()를 통하여 JSON을 변환해주는 컨버터도 추가해주고 build()를 통해 생성
+             */
+            val retrofit = Retrofit.Builder()
+                .baseUrl("https://api.icndb.com/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+            /**
+             * retrofit 객체를 통해 인터페이스를 실행가능한 서비스객체 생성
+             * 인터페이스에 정의된 API 엔드포인트들의 구현체를 만든다
+             */
+            val jokeService = retrofit.create(JokeService::class.java)
+
+            jokeService.getData("random").enqueue(object: Callback<JokeResponse> {
+                override fun onFailure(call: Call<JokeResponse>, t: Throwable) {
+                    Log.d("Response Error ::", "Failed API call with call: " + call +
+                            " + exception: " + t)
+                }
+
+                override fun onResponse(
+                    call: Call<JokeResponse>,
+                    response: Response<JokeResponse>
+                ) {
+                    Log.d("Response :: ", "${response.body()}")
+                    if (response.isSuccessful) {
+                        textView.append(response.body()?.data.toString())
+                    }
+                }
+            })
+
+        } catch (e: Exception) {
+            throw e
+        }
+    }
+
+    private fun getDataByURLConnection(url: URL) {
+        try {
+            thread {
+//                서버와 연결 생성 openConnection의 반환값이 URLConnection 클래스인데 추상클래스이므로 실제 구현 클래스로 형변환
+                val httpsConn = url.openConnection() as HttpsURLConnection
+                httpsConn.requestMethod = "GET"
+//                응답 코드가 200으로 정상일때
+                if (httpsConn.responseCode == HttpsURLConnection.HTTP_OK) {
+//                    데이터를 읽어오는 스트림을 연결하여 버퍼에 담는것
+                    val streamReader = InputStreamReader(httpsConn.inputStream)
+                    val resBuffer = BufferedReader(streamReader)
+
+
+                    while (true) {
+                        val resLine = resBuffer.readLine() ?: break
+                        Log.d("RESULT :: ", resLine)
+                        runOnUiThread {
+                            textView.append(resLine)
+                        }
+                    }
+//                        스트림과 커넥션 해제
+                    resBuffer.close()
+                    httpsConn.disconnect()
+
+                }
+
+            }
+        } catch (e: Exception) {
+            throw e
+        }
     }
 }
